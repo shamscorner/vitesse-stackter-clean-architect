e9ecd69af6cef251b2ca5f65fe5bc8a2c763fd1c
Shamim Hossain <hossains159@gmail.com>
2022-03-04 19:19:22 +0600

    chore: change map to foreach to load modules

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     // install all modules under `**/modules/`
-    Object.values(import.meta.globEager('./**/modules/*.ts')).map(i => i.install?.(ctx))
+    Object.values(import.meta.globEager('./**/modules/*.ts')).forEach(i => i.install?.(ctx))
   },
 )
